87e356abc3cc8f110ce02ed4f8ae7a627e677a4a
pramaths <103276292+pramaths@users.noreply.github.com>
2024-03-06 17:38:29 +0530

    first

--- app/graph/page.jsx
+++ app/graph/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from "next/image";
-import Graph from "../components/graph"
+import Graph from "../../components/graph"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
