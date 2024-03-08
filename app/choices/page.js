3cb00b6dd8a17fb5d579ca408f9635e0ec0ac617
pramaths <103276292+pramaths@users.noreply.github.com>
2024-03-08 16:23:51 +0530

    choices

--- app/choices/page.js
+++ app/choices/page.js
@@ -1,10 +1,12 @@
 'use client'
 import Image from "next/image";
 import Choices from "../../components/Choices"
+import Options from "../../components/component/optionsCard"
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Choices/>
+    <main className="flex min-h-screen w-full flex-col items-center justify-between">
+      {/* <Choices/> */}
+      <Options/>
     </main>
   );
 }
